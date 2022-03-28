af257108f0e3d051186e24bac201bb2e8fc0e1f8
MrTan <freemrtan@gmail.com>
2022-03-28 22:03:58 +0800

    add pages

--- front/firm-front/src/views/HomeView.vue
+++ front/firm-front/src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import Grid from "vue-virtual-scroll-grid";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  
 </template>
---
+++ front/firm-front/src/views/Login.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const mail = ref("");
+const password = ref("");
+
+function login() {}
+</script>
+
+<template>
+  <div>
+    <label for="mail">Email</label>
+    <input id="mail" v-model="mail" />
+    <label for="password">Password</label>
+    <input id="password" v-model="password" />
+    <button @click="login">Login</button>
+  </div>
+</template>
