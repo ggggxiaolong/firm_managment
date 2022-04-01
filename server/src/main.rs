a921abf7a968ffe42076da2f74cec80552aa147a
MrTan <freemrtan@gmail.com>
2022-04-01 22:21:42 +0800

    add web

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,19 +4,18 @@ extern crate thiserror;
 
 use controller::Api;
 use poem::{
-    listener::TcpListener, middleware::Cors, EndpointExt,
-    Result, Route, Server,
+    endpoint::StaticFilesEndpoint, listener::TcpListener, middleware::Cors, EndpointExt, Result,
+    Route, Server,
 };
-use poem_openapi::{
-    OpenApiService,
-};
+use poem_openapi::OpenApiService;
 
 pub mod controller;
 // pub mod dao;
 pub mod domain;
 pub mod service;
 pub mod utils;
-type DbPool = sqlx::SqlitePool;#[tokio::main]
+type DbPool = sqlx::SqlitePool;
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "poem=debug");
@@ -24,14 +23,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // env_logger::init();
     tracing_subscriber::fmt::init();
     let pool = DbPool::connect("sqlite://firm.db").await?;
-    let api_service =
-        OpenApiService::new(Api, "Firm Api", "1.0.0").server("http://0.0.0.0:3000");
-    let ui = api_service.swagger_ui();
-    let spec = api_service.spec();
+    let api_service = OpenApiService::new(Api, "Firm Api", "1.0.0").server("http://0.0.0.0:3000");
+    // let ui = api_service.swagger_ui();
+    // let spec = api_service.spec();
     let route = Route::new()
+        .nest("/", StaticFilesEndpoint::new("./dist").index_file("index.html"))
         .nest("/api", api_service)
-        .nest("/ui", ui)
-        .at("/spec", poem::endpoint::make_sync(move |_| spec.clone()))
+        // .nest("/ui", ui)
+        // .at("/spec", poem::endpoint::make_sync(move |_| spec.clone()))
         .with(Cors::new())
         .data(pool);
 
